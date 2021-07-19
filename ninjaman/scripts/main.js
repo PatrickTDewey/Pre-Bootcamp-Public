b25585e855a2516e9178341f6f717dd839aa29c8
PatrickTDewey <ptddev@protonmail.com>
2021-07-18 18:11:59 -0700

    added start to pacman game

---
+++ ninjaman/scripts/main.js
@@ -0,0 +1,16 @@
+// function makeWorld(grid) {
+//     let x = grid, y = grid;
+//     for (let length = 0; length < y; length++) {
+//         console.log(`length: ` + length);
+//         let row = document.createElement('row');
+        
+//         for (let width = 0; width < x; width++) {
+//             let wall = document.createElement('wall');
+            
+//             console.log(`width: ` + width);
+            
+//         }
+//     }
+//     document.body.appendChild(row);
+// }
+// makeWorld(8);
